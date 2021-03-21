585f6832b0fd97248df31fd1a82a609295cf6b61
Maxim Vainikka <rithmique@yahoo.com>
2021-03-21 19:20:00 +0100

    update viewcontroller

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -19,15 +19,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondAction: UIButton!
     
     @IBAction func choiceButton(_ sender: UIButton) {
+        let choice = sender.currentTitle
+        if choice == story[storyNumber].choice1 {
+            storyNumber = 2
+            updateStory()
+        }else if choice == story[storyNumber].choice2 {
+            storyNumber = 1
+            updateStory()
+        }
     }
     
     var story = [Story(t: "One day she had thinking about him at the new year holidays", ch1: "Write him", ch2: "Do nothing"),
-                 Story(t: <#T##String#>, ch1: <#T##String#>, ch2: <#T##String#>)]
+                 Story(t: "ez", ch1: "not ez", ch2: "truly ez"),
+                 Story(t: "sad", ch1: "not sad", ch2: "truly sad")]
     var storyNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        updateStory()
+    }
+    func updateStory() {
+        historyLabel.text = "History root: \(storyNumber)"
         storyLabel.text = story[storyNumber].title
         firstAction.setTitle(story[storyNumber].choice1, for: .normal)
         secondAction.setTitle(story[storyNumber].choice2, for: .normal)
