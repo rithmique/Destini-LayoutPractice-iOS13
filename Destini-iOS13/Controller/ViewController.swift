72dfc772d99228912974aac7f32b7f7f92ac5648
Maxim Vainikka <rithmique@yahoo.com>
2021-03-21 14:45:37 +0100

    1.0
    init repo

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var historyLabel: UILabel!
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    
+    @IBOutlet weak var firstAction: UIButton!
+    
+    @IBOutlet weak var secondAction: UIButton!
+    
+    @IBAction func choiceButton(_ sender: UIButton) {
+    }
+    
+    var story = [Story(t: "One day she had thinking about him at the new year holidays", ch1: "Write him", ch2: "Do nothing"),
+                 Story(t: <#T##String#>, ch1: <#T##String#>, ch2: <#T##String#>)]
+    var storyNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        storyLabel.text = story[storyNumber].title
+        firstAction.setTitle(story[storyNumber].choice1, for: .normal)
+        secondAction.setTitle(story[storyNumber].choice2, for: .normal)
     }
-
-
+    
+    
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+struct Story {
+    var title: String
+    var choice1: String
+    var choice2: String
+    
+    init(t: String, ch1: String, ch2: String){
+        self.title = t
+        self.choice1 = ch1
+        self.choice2 = ch2
+    }
+    func getStory() -> String {
+        return title
+    }
+    func getChoice1() -> String {
+        return choice1
+    }
+    func getChoice2() -> String {
+        return choice2
+    }
+}
